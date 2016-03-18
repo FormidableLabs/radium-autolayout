ce651f9c216c60b465be0eb71d26999b173d6450
Tyler Thompson <tyler@tylerpaulthompson.com>
2016-03-18 14:43:42 -0700

    Export AutoSVG

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import ConstraintLayout from "./constraint-layout";
 import View from "./view";
 import Subview from "./subview";
-import AutoDOM from "./autodom.js";
-import constrain from "./constraint-builder.js";
+import AutoDOM from "./autodom";
+import AutoSVG from "./autosvg";
+import constrain from "./constraint-builder";
 
 export default ConstraintLayout;
-export { View, Subview, AutoDOM, constrain };
+export { View, Subview, AutoDOM, AutoSVG, constrain };
